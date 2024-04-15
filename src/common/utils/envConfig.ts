ab5baad0b418a8e2a9e17a1f1c015d64d1249eef
Martin Bogner <martin.bogner@aparavi.com>
2024-04-15 10:22:47 +0000

    default set up dev .env

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { cleanEnv, host, num, port, str, testOnly } from 'envalid';
 
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 export const env = cleanEnv(process.env, {
   NODE_ENV: str({ devDefault: testOnly('test'), choices: ['development', 'production', 'test'] }),
